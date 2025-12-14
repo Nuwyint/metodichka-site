2984a9f7315da412776b82924285afc0f87b193e
Nuwyint <sahamain23@gmail.com>
2025-12-14 22:01:18 +0300

    fix: vercerl base error

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  // GitHub Pages: сайт находится в подпапке репозитория
-  // https://<user>.github.io/<repo>/
-  base: '/metodichka-site/',
   plugins: [react()],
-})
+  base: "./", // имя репозитория
+});
